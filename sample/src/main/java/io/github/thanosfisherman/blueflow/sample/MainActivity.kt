d8e1cc02441b76f097aa139427216db71af5db9c
Thanos Psaridis <psaridis@gmail.com>
2020-01-13 09:57:11 +0200

    added safeCollect to prevent flow from leaking

--- sample/src/main/java/io/github/thanosfisherman/blueflow/sample/MainActivity.kt
+++ sample/src/main/java/io/github/thanosfisherman/blueflow/sample/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,4 +37,15 @@ class MainActivity : AppCompatActivity() {
         return pls
     }
 
+}
+/**
+ * Only proceed with the given action if the coroutine has not been cancelled.
+ * Necessary because Flow.collect receives items even after coroutine was cancelled
+ * https://github.com/Kotlin/kotlinx.coroutines/issues/1265
+ */
+suspend inline fun <T> Flow<T>.safeCollect(crossinline action: suspend (T) -> Unit) {
+    collect {
+        coroutineContext.ensureActive()
+        action(it)
+    }
 }
