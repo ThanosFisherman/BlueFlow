d52d41e1a208c562ff23b9f77f7c5a1df7fce776
Thanos Psaridis <psaridis@gmail.com>
2020-01-17 16:54:14 +0200

    Fooling around with sample

--- sample/src/main/java/io/github/thanosfisherman/blueflow/sample/MainActivity.kt
+++ sample/src/main/java/io/github/thanosfisherman/blueflow/sample/MainActivity.kt
@@ -1,49 +1,105 @@
 package io.github.thanosfisherman.blueflow.sample
 
+import android.Manifest
+import android.bluetooth.BluetoothDevice
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.github.thanosfisherman.blueflow.BlueFlow
+import io.github.thanosfisherman.blueflow.safeCollect
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
-import kotlin.coroutines.coroutineContext
+
+private const val REQUEST_PERMISSION_COARSE_LOCATION = 0
+private const val REQUEST_ENABLE_BT = 1
 
 class MainActivity : AppCompatActivity() {
+
+    private val devices = arrayListOf<BluetoothDevice>()
+    private lateinit var blueFlow: BlueFlow
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        blueFlow = BlueFlow(applicationContext)
+        start.setOnClickListener {
+            devices.clear()
+            setAdapter(devices)
 
-        runBlocking {
-            createFlow()
-            createFlow().buffer().safeCollect() {
-                Log.i("Main", it.toString())
-                if (it == 1) {
-                    cancel()
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    REQUEST_PERMISSION_COARSE_LOCATION
+                )
+            } else {
+                blueFlow.startDiscovery()
+                var job = observeDevices()
+                if (job.isActive) {
+                    job.cancel()
+                    job = observeDevices()
                 }
             }
-
         }
     }
 
-    private suspend fun createFlow(): Flow<Int> {
+    @ExperimentalCoroutinesApi
+    fun observeDevices(): Job {
 
-        return flow {
-            Log.i("MAIN", "createFlow Called")
-            emit(1)
-            emit(2)
+        val uiScope = CoroutineScope(Dispatchers.Main)
+
+        return uiScope.launch {
+            blueFlow.discoverDevices().safeCollect {
+                addDevice(it)
+                Log.i("MAIN", it.address)
+            }
         }
     }
 
-}
-/**
- * Only proceed with the given action if the coroutine has not been cancelled.
- * Necessary because Flow.collect receives items even after coroutine was cancelled
- * https://github.com/Kotlin/kotlinx.coroutines/issues/1265
- */
-suspend inline fun <T> Flow<T>.safeCollect(crossinline action: suspend (T) -> Unit) {
-    collect {
-        coroutineContext.ensureActive()
-        action(it)
+    private fun addDevice(device: BluetoothDevice) {
+        devices.add(device)
+        setAdapter(devices)
+    }
+
+    private fun setAdapter(list: List<BluetoothDevice>) {
+        val itemLayoutId = android.R.layout.simple_list_item_1
+        result.adapter = object : ArrayAdapter<BluetoothDevice?>(
+            this, android.R.layout.simple_list_item_2,
+            android.R.id.text1, list
+        ) {
+            override fun getView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view = super.getView(position, convertView, parent)
+                val device: BluetoothDevice = devices[position]
+                var devName = device.name
+                val devAddress = device.address
+                if (TextUtils.isEmpty(devName)) {
+                    devName = "NO NAME"
+                }
+                val text1 =
+                    view.findViewById(android.R.id.text1) as TextView
+                val text2 =
+                    view.findViewById(android.R.id.text2) as TextView
+                text1.text = devName
+                text2.text = devAddress
+                return view
+            }
+        }
     }
 }
