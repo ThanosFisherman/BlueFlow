c94a930efcf1d5066c4e30f1cda77973cb8bfd6e
Thanos Psaridis <psaridis@gmail.com>
2020-01-22 13:33:15 +0200

    handy extension functions

--- blueflowlib/src/main/java/io/github/thanosfisherman/blueflow/BluetoothExt.kt
+++ blueflowlib/src/main/java/io/github/thanosfisherman/blueflow/BluetoothExt.kt
@@ -1,7 +1,14 @@
 package io.github.thanosfisherman.blueflow
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.Context
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
+import java.util.*
 
 fun BluetoothDevice.createRfcommSocket(channel: Int): BluetoothSocket {
     try {
@@ -11,3 +18,30 @@ fun BluetoothDevice.createRfcommSocket(channel: Int): BluetoothSocket {
         throw UnsupportedOperationException(e)
     }
 }
+
+@ExperimentalCoroutinesApi
+fun BluetoothAdapter.discoverDevices(context: Context) =
+    BlueFlow.getInstance(context).discoverDevices()
+
+suspend fun BluetoothDevice.connectAsClientAsync(uuid: UUID, secure: Boolean = true) =
+    coroutineScope {
+        return@coroutineScope async(Dispatchers.IO) {
+            val bluetoothSocket =
+                if (secure) createRfcommSocketToServiceRecord(uuid)
+                else createInsecureRfcommSocketToServiceRecord(uuid)
+            bluetoothSocket.also { it.connect() }
+        }
+    }
+
+@ExperimentalCoroutinesApi
+fun BluetoothSocket.readByteStream() = channelFlow {
+    while (isActive) {
+        try {
+            offer(inputStream.read().toByte())
+        } catch (e: IOException) {
+            error("Couldn't read bytes from flow. Disconnected")
+        }
+    }
+}.flowOn(Dispatchers.IO)
+
+fun BluetoothSocket.readByteStream(readInterceptor: (ByteArray) -> Boolean): Nothing = TODO()
