1e0306e94efbefc7f86cf953d6279ee050909f7b
Thanos Psaridis <psaridis@gmail.com>
2020-06-13 11:40:02 +0300

    Release 1.1.1 for real

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val KOTLIN = "1.3.72"
     const val BINTRAY = "1.8.5"
-    const val Coroutines = "1.3.6"
+    const val Coroutines = "1.3.7"
     const val KTX = "1.3.0-rc01"
     const val AppCompat = "1.1.0"
     const val DOKKA = "0.10.1"
